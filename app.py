e399da520f0429aebd2b416869f5d77270ed2c57
marekkomp <195009940+marekkomp@users.noreply.github.com>
2025-03-06 07:38:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-# Wczytanie pliku CSV
-uploaded_file = st.file_uploader("Wybierz plik CSV", type=["csv"])
+# Wybór pliku (CSV lub Excel)
+uploaded_file = st.file_uploader("Wybierz plik CSV lub Excel", type=["csv", "xlsx"])
 
 if uploaded_file is not None:
-    # Wczytanie danych do DataFrame
-    df = pd.read_csv(uploaded_file)
+    # Sprawdzenie, czy plik to CSV czy Excel
+    if uploaded_file.name.endswith('.csv'):
+        # Wczytanie pliku CSV
+        df = pd.read_csv(uploaded_file)
+    elif uploaded_file.name.endswith('.xlsx'):
+        # Wczytanie pliku Excel
+        df = pd.read_excel(uploaded_file)
 
     # Wyświetlenie surowych danych
-    st.write("Dane z pliku CSV:", df)
+    st.write("Dane z pliku:", df)
 
-    # Grupowanie po kolumnie "tags" i obliczanie sumy w pozostałych kolumnach (np. jeśli są numeryczne)
+    # Grupowanie po kolumnie "tags" i obliczanie sumy w pozostałych kolumnach
     if 'tags' in df.columns:
         grouped_data = df.groupby('tags').sum()
-
         # Wyświetlenie pogrupowanych danych
         st.write("Dane pogrupowane po modelach (tags):", grouped_data)
-
     else:
-        st.warning("Brak kolumny 'tags' w pliku CSV.")
+        st.warning("Brak kolumny 'tags' w pliku.")
